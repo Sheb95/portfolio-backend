40a8c39cbc4d009ce17c5dfad918ec7cef8de8ab
Sheb95 <80277508+Sheb95@users.noreply.github.com>
2022-04-07 14:12:17 +0100

    started to add projects into data file

---
+++ libs/ProjectData.js
@@ -0,0 +1,44 @@
+const Projects = [
+   {
+      id: 1,
+      title: "Plethora: A School of Code Bootcamper Resources Collection",
+      tech: [
+         "JavaScript",
+         "Postman",
+         "React",
+         "CSS",
+         "HTML",
+         "PostgreSQL",
+         "Express",
+         "Node.js",
+      ],
+      github:
+         "https://github.com/SchoolOfCode/national-project-week-repo-2-room29-chirag-sheban-kate",
+      description:
+         "My team and I were given a week to produce an application that solved the School of Code students' problems. One of the issues we found was access to recommended resources. Thus, we created a curated platform where students can search for resources useful to others. I led my team through the backend development of our website, created a RESTAPI using Express and PostgreSQL and deployed it on Heroku and conducted manual testing of API endpoints using Postman. ",
+      url: "https://resourceapi.netlify.app/",
+      isDeployed: true,
+   },
+   {
+      id: 2,
+      title: "WeShare",
+      tech: [
+         "JavaScript",
+         "Postman",
+         "Next.js",
+         "CSS",
+         "HTML",
+         "Prisma",
+         "Express",
+         "Node.js",
+         "MaterialMUI",
+         "Jest",
+         "Cypress.io",
+      ],
+      github:
+         "https://github.com/SchoolOfCode/final-project-repo-easy-scrum-easy-go-room-21",
+      description: "",
+      url: "https://servicestack.netlify.app/",
+      isDeployed: true,
+   },
+];
